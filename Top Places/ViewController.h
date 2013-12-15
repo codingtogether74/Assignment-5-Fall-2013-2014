4d5390e17a0036c56e374395545036d3f736d4ea
Tatiana Kornilova <tatiana.kornilova@gmail.com>
2013-12-15 11:50:25 +0000

    Initial Commit

---
+++ Top Places/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Top Places
+//
+//  Created by Tatiana Kornilova on 12/15/13.
+//  Copyright (c) 2013 Tatiana Kornilova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Top Places/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Top Places
+//
+//  Created by Tatiana Kornilova on 12/15/13.
+//  Copyright (c) 2013 Tatiana Kornilova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
